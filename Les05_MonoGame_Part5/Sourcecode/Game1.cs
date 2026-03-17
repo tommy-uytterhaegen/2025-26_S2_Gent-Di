38e0f9b5d7cd8d3dbd096b0303a9b0758752cc17
Tommy <tommy.uytterhaegen@gmail.com>
2026-03-17 11:22:37 +0100

    Initial

---
+++ Les05_MonoGame_Part5/Sourcecode/Game1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame_Pikachu.Extensions;
+using MonoGame_Pikachu.States;
+using System;
+using System.Collections.Generic;
+
+namespace MonoGame_Pikachu
+{
+    // Ik heb mogelijke TODO's toegevoegd die het spel interessanter kunnen maken. Deze zijn vrijblijvend, maar aangeraden. 
+    public class Game1 : Game
+    {
+        // Constants for game configuration
+        public const int SHARK_SPAWN_TIME_IN_MS = 3000;
+
+        public const float PLAYER_SPEED = 5f;
+        public const float PLAYER_SCALE = 1.0f;
+
+        public const float SHARK_SPEED = 3f;
+        public const float SHARK_SCALE = 0.35f;
+
+        public const float BACKGROUND_SPEED = 2f;
+        public const float BACKGROUND_SCALE = 0.25f;
+
+        private AbstractState _activeState;
+
+        private SpriteBatch _spriteBatch;
+
+        // DIT IS VUIL
+        // We willen iedere 3 seconden een nieuwe haai, hiervoor moeten wij bijhouden hoeveel tijd er is verstreken sinds de laatste haai is gespawned. Hiervoor gebruiken wij deze variabele.
+        public double _elapsedTimeForSpawnInMs;
+
+        public GraphicsDeviceManager _graphics;
+
+        public SpriteFont _spriteFont;
+
+        public Texture2D _playerTexture;
+        public Vector2 _playerPosition;
+
+        public Texture2D _enemyTexture;
+        public List<Vector2> _enemyPositions;
+
+        public Texture2D _backgroundTexture;
+        public Vector2 _backgroundPosition;
+        // EINDE VUIL
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            // Resolutie verzetten
+            _graphics.PreferredBackBufferHeight = 540;
+            _graphics.PreferredBackBufferWidth = 768;
+            _graphics.ApplyChanges();
+        }
+
+        public void ChangeState(AbstractState newActiveActive)
+        {
+            _activeState = newActiveActive;
+        }
+
+        protected override void Initialize()
+        {
+            _activeState = new StartScreenState(this);
+
+            _elapsedTimeForSpawnInMs = 0;
+
+            _playerPosition = new Vector2(0, _graphics.PreferredBackBufferHeight / 2);
+            _backgroundPosition = new Vector2(0, 0);
+            _enemyPositions = new List<Vector2>();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _playerTexture = Content.Load<Texture2D>("surfing-pikachu");
+            _enemyTexture = Content.Load<Texture2D>("haai");
+            _backgroundTexture = Content.Load<Texture2D>("water");
+
+            _spriteFont = Content.Load<SpriteFont>("game-font");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _activeState.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // TODO: Afhankelijk van het aantal haaien die onze speler voorbij is zouden we de clear kleur kunnen aanpassen, zo wordt het spel steeds donkerder naarmate je meer haaien voorbij bent. Nu is de clear kleur altijd hetzelfde.
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _activeState.Draw(gameTime, _spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+
+    }
+}
---
+++ Les05_MonoGame_Part5/Sourcecode/States/PlayingState.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame_Pikachu.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame_Pikachu.States
+{
+    public class PlayingState
+        : AbstractState
+    {
+        public PlayingState(Game1 context) 
+            : base(context)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Controlleer of de speler een haai aanraakt. Indien 'ja' -> Dood (bv. Exit)
+
+            SpawnEnemyIfNeeded(gameTime);
+
+            UpdateBackgroundPosition();
+
+            HandlePlayerMovement();
+
+            UpdateEnemyPositions();
+
+            // TODO: Als een haai links uit beeld is, dan mag deze uit de lijst. Nu blijven de haaien oneindig in de lijst staan, ook al zijn ze al lang uit beeld.
+
+            if (IsKeyDown(Keys.P))
+                Context.ChangeState(new PausedState(Context, this));
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Context._backgroundTexture, Context._backgroundPosition, Game1.BACKGROUND_SCALE);
+            spriteBatch.Draw(Context._playerTexture, Context._playerPosition, Game1.PLAYER_SCALE);
+
+            // TODO: We zouden bij het spawned van de haaien een bepaalde random scale kunnen geven aan een specifieke haai, zo zien sommige er groter uit dan anderen. Dit zou het speelveld interessanter maken. Nu hebben alle haaien dezelfde grootte.
+            foreach (var sharkPosition in Context._enemyPositions)
+                spriteBatch.Draw(Context._enemyTexture, sharkPosition, Game1.SHARK_SCALE);
+        }
+
+        private void SpawnEnemyIfNeeded(GameTime gameTime)
+        {
+            // TODO: Er is nog geen limiet aan het aantal haaien dat kan spawnen. Voeg een limiet toe van 8 haaien tegelijk.
+            Context._elapsedTimeForSpawnInMs += gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (Context._elapsedTimeForSpawnInMs >= Game1.SHARK_SPAWN_TIME_IN_MS)
+            {
+                // TODO: Geef een haai ook verschillende snelheden, nu hebben alle haaien dezelfde snelheid (wat het speelveld saai maakt). Je kan bijvoorbeeld een random snelheid geven tussen de 1 en 3.
+                // TODO: Spawn elke haai op een random hoogte, nu spawnen ze altijd in het midden van het scherm.
+                Context._enemyPositions.Add(new Vector2(Context._graphics.PreferredBackBufferWidth, Context._graphics.PreferredBackBufferHeight / 2));
+                Context._elapsedTimeForSpawnInMs = 0;
+            }
+        }
+
+        private void UpdateEnemyPositions()
+        {
+            for (var i = 0; i < Context._enemyPositions.Count; i++)
+                Context._enemyPositions[i] = Context._enemyPositions[i] with { X = Context._enemyPositions[i].X - Game1.SHARK_SPEED };
+        }
+
+        private void UpdateBackgroundPosition()
+        {
+            Context._backgroundPosition.X -= Game1.BACKGROUND_SPEED;
+        }
+
+        private void HandlePlayerMovement()
+        {
+            // TODO: We zouden deze toetsen kunnen uitlezen uit een bestand (denk aan wat jullie bij programmeren gedaan hebben). Dit zou het makkelijker maken om de controls aan te passen, zonder dat je de code moet aanpassen. Nu zijn de controls hardcoded in de code.
+            if (ShouldMoveRight())
+                MoveRight();
+
+            // TODO: Splits deze ook af zoals de MoveRight / ShouldMoveRight
+            if (IsKeyDown(Keys.Left, Keys.Q))
+                Context._playerPosition.X -= Game1.PLAYER_SPEED;
+
+            if (IsKeyDown(Keys.Up, Keys.Z))
+                Context._playerPosition.Y -= Game1.PLAYER_SPEED;
+
+            if (IsKeyDown(Keys.Down, Keys.S))
+                Context._playerPosition.Y += Game1.PLAYER_SPEED;
+        }
+
+        private void MoveRight()
+        {
+            Context._playerPosition.X += Game1.PLAYER_SPEED;
+        }
+
+        private bool ShouldMoveRight()
+        {
+            return IsKeyDown(Keys.Right, Keys.D);
+        }
+
+    }
+}
